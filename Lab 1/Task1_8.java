773941b0c6b1da26989684556e0f1b6f8ef1fbb5
Slava206 <slava.tepaev@yandex.ru>
2025-09-23 23:04:22 +0300

    Create Task1_8.java

---
+++ Lab 1/Task1_8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task1_4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(average(a, b, c));
+        scan.close();
+    }
+    
+    public static int average(int a, int b, int c){
+        return (a + b + c) / 3;
+    }
+}
